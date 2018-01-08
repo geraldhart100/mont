756e7e86c13132ed4d2aacf49ce203c632f4df4b
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-01-08 21:12:30 +0400

    Remove index mw tests

--- test/middlewares.spec.js
+++ test/middlewares.spec.js
@@ -17,48 +17,6 @@ test.beforeEach(async t => {
   t.context = { db }
 })
 
-test('ensure indexes', async t => {
-  const { db } = t.context
-
-  const options = {
-    indexes: [
-      {
-        key: {
-          id: 1,
-          type: 1
-        },
-        unique: true
-      }
-    ]
-  }
-
-  const col = db.get('indexxx', options)
-
-  await col
-    .indexes()
-    .then(indexes => {
-      const index = indexes.id_1_type_1
-      t.not(index, undefined, 'accept indexes array')
-    })
-
-  await col
-    .dropIndexes()
-    .then(col.indexes)
-    .then(indexes => {
-      const index = indexes.id_1_type_1
-      t.is(index, undefined, 'runs once on init')
-    })
-
-  const col2 = db.get('index-' + Date.now())
-
-  await col
-    .insert({})
-    .then(res => {
-      t.pass('works fine with no indexes')
-    })
-})
-
-
 test('options > should allow defaults', async t => {
   const { db } = t.context
 
