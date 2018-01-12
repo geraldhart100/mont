e34e3c7984b9bd4683d1de8574b0e6c78698baab
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-01-12 08:18:32 +0400

    Move refs to core.json

--- middlewares/ensure-schema/lib/schema.js
+++ middlewares/ensure-schema/lib/schema.js
@@ -2,8 +2,10 @@ const sqem = require('sqem')
 
 const env = require('sqem/lib/ajv')
 
+const core = require('../schemas/core')
 const entity = require('../schemas/entity')
 
+env.addSchema(core)
 env.addSchema(entity)
 
 module.exports.validate = sqem
