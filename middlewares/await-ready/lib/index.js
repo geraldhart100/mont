eaa3417e83d5f240aab2d0ba0660f9016c1c21b6
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-01-14 09:23:46 +0400

    Update await-ready name

--- middlewares/await-ready/lib/index.js
+++ middlewares/await-ready/lib/index.js
@@ -1,19 +1,17 @@
-function awaitReady () {
-  return (ctx, next) => {
-    const { manager, collection } = ctx
+function awaitReady (ctx, next) {
+  const { manager, collection } = ctx
 
-    const getCol = db => db.collection(collection.name)
+  const getCol = db => db.collection(collection.name)
 
-    const resolve = col => {
-      ctx.col = col
-      return next()
-    }
-
-    return manager
-      .resolveDb()
-      .then(getCol)
-      .then(resolve)
+  const resolve = col => {
+    ctx.col = col
+    return next()
   }
+
+  return manager
+    .resolveDb()
+    .then(getCol)
+    .then(resolve)
 }
 
-module.exports = awaitReady
+module.exports = () => awaitReady
