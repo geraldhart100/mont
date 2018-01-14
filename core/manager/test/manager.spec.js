bf48f80a5b90a853c1f56334fe65771a9590b03a
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-01-14 16:56:19 +0400

    TOuch

--- core/manager/test/manager.spec.js
+++ core/manager/test/manager.spec.js
@@ -4,6 +4,9 @@ import mongodb from 'mongodb'
 
 import { MongoDBServer } from 'mongomem'
 
+import Connection from 'mont-connection'
+import Collection from 'mont-collection'
+
 import Manager from '..'
 
 test.before(MongoDBServer.start)
@@ -12,11 +15,18 @@ test.beforeEach(async t => {
   t.context.uri = await MongoDBServer.getConnectionString()
 })
 
+test('constructor', async t => {
+  const { uri } = t.context
+  const manager = new Manager(uri)
+
+  t.true(manager instanceof Connection)
+})
+
 test('collection', async t => {
   const { uri } = t.context
   const manager = new Manager(uri)
 
   const col = manager.get('users')
 
-  t.truthy(col)
+  t.true(col instanceof Collection)
 })
