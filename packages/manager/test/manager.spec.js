fd4c32107170803f7f5c38ff702e0af9291e93fc
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-01-16 05:30:10 +0400

    Fix manager tests

--- packages/manager/test/manager.spec.js
+++ packages/manager/test/manager.spec.js
@@ -1,30 +1,11 @@
 import test from 'ava'
 
-import mongodb from 'mongodb'
-
-import { MongoDBServer } from 'mongomem'
-
-import Connection from 'mont-connection'
 import Collection from 'mont-collection'
 
 import Manager from '..'
 
-test.before(MongoDBServer.start)
-
-test.beforeEach(async t => {
-  t.context.uri = await MongoDBServer.getConnectionString()
-})
-
-test('constructor', async t => {
-  const { uri } = t.context
-  const manager = new Manager(uri)
-
-  t.true(manager instanceof Connection)
-})
-
 test('collection', async t => {
-  const { uri } = t.context
-  const manager = new Manager(uri)
+  const manager = new Manager('localhost/app')
 
   const col = manager.get('users')
 
