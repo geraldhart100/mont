18c1fced17ad7cc442ee963764c27cc85c5e2f01
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-01-16 05:19:59 +0400

    Remove package mont

--- packages/mont/lib/index.js
+++
@@ -1,7 +0,0 @@
-const Manager = require('mont-manager')
-
-function Mont (uri, opts) {
-  return new Manager(uri, opts)
-}
-
-module.exports = Mont
--- packages/mont/test/mont.spec.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava'
-
-import mont from '..'
-
-test('signature', t => {
-  t.notThrows(() => mont('localhost/db'), 'init w/o `new`')
-})
-
